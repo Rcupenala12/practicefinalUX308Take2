48193af99d2e64e0c2be014eafa405a6d8f66d44
Rcupenala12 <145622263+Rcupenala12@users.noreply.github.com>
2025-04-04 01:10:05 +0000

    add both

--- src/components.jsx
+++ src/components.jsx
@@ -1,4 +1,4 @@
-import {hello} from './functions.js';
+import {hello} from fahrenheitToCelsius './functions.js';
 
 function Question1(){
     return <section>
@@ -10,4 +10,13 @@ function Question1(){
     </section>;
 }
 
-export {Question1}
+function Question2() (
+    return (
+        <section>
+            <h2>Results</h2>
+            <p>fahrenheitToCelsius(32) == {fahrenheitToCelsius(32)}</p>
+            <p>fahrenheitToCelsius(212) == {fahrenheitToCelsius(212)}</p>
+            <p>fahrenheitToCelsius(70) == {fahrenheitToCelsius(70)}</p>
+        </section>
+
+export {Question1,Question2}
--- src/functions.js
+++ src/functions.js
@@ -2,4 +2,12 @@ function hello(name){
     return(`hello ${name}`);
 }
 
+
+function fahrenheitToCelsius (fahrenheit){
+    const celsius = (fahrenheit -32) * 5/9
+    return celsius;
+    110
+    export
+    (hello, fahrenheitToCelsius)
+
 export {hello}
